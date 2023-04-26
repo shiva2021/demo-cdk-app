9227d991950e3b6ca00a1c5ebd4947288ed1331a
shiva2021 <shiva.chaturvedi91@gmail.com>
2023-04-26 20:52:53 +0530

    linting changes

--- lib/test-cdk-app-stack.ts
+++ lib/test-cdk-app-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { aws_lambda as lambda, aws_iam as iam , aws_logs as logs} from "aws-cdk-lib";
+import { aws_lambda as lambda, aws_iam as iam, aws_logs as logs } from "aws-cdk-lib";
 
 export class TestCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -18,6 +18,6 @@ export class TestCdkAppStack extends cdk.Stack {
         CRAZY_MESSAGE: "Dogs can fly!!",
       },
       logRetention: logs.RetentionDays.ONE_DAY,
-    })
+    });
   }
 }
--- test/test-cdk-app.test.ts
+++ test/test-cdk-app.test.ts
@@ -5,20 +5,19 @@ import { TestCdkAppStack } from "../lib/test-cdk-app-stack";
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/test-cdk-app-stack.ts
 test("SQS Queue Created", () => {
-    const app = new cdk.App();
+  const app = new cdk.App();
 
-    const testCdkAppStack = new TestCdkAppStack(app, "TestCdkAppStack");
+  const testCdkAppStack = new TestCdkAppStack(app, "TestCdkAppStack");
 
-    const template = Template.fromStack(testCdkAppStack);
+  const template = Template.fromStack(testCdkAppStack);
 
-    template.hasResourceProperties("AWS::Lambda::Function", {
-        Handler: "index.handler",
-        Runtime: "nodejs16.x",
-      });
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Handler: "index.handler",
+    Runtime: "nodejs16.x",
+  });
 
-    template.resourceCountIs("AWS::Lambda::Function", 2);
+  template.resourceCountIs("AWS::Lambda::Function", 2);
 });
-
 
 /** 
 By setting the logRetention prop for our lambda function, 
