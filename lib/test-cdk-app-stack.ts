6c467569e322a245e3cbbc3694e9ee44865ff2d2
shiva2021 <shiva.chaturvedi91@gmail.com>
2023-04-26 20:52:32 +0530

    demo app implementation

--- lib/test-cdk-app-stack.ts
+++ lib/test-cdk-app-stack.ts
@@ -1,6 +1,6 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_lambda as lambda, aws_iam as iam , aws_logs as logs} from "aws-cdk-lib";
 
 export class TestCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +8,16 @@ export class TestCdkAppStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new lambda.Function(this, "HelloWorldFunc", {
+      code: lambda.Code.fromAsset("HelloWorldFunction"),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_16_X,
+      timeout: cdk.Duration.seconds(5),
+      memorySize: 1024,
+      environment: {
+        CRAZY_MESSAGE: "Dogs can fly!!",
+      },
+      logRetention: logs.RetentionDays.ONE_DAY,
+    })
   }
 }
--- test/test-cdk-app.test.ts
+++ test/test-cdk-app.test.ts
@@ -1,17 +1,35 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as TestCdkApp from '../lib/test-cdk-app-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { TestCdkAppStack } from "../lib/test-cdk-app-stack";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/test-cdk-app-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new TestCdkApp.TestCdkAppStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("SQS Queue Created", () => {
+    const app = new cdk.App();
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    const testCdkAppStack = new TestCdkAppStack(app, "TestCdkAppStack");
+
+    const template = Template.fromStack(testCdkAppStack);
+
+    template.hasResourceProperties("AWS::Lambda::Function", {
+        Handler: "index.handler",
+        Runtime: "nodejs16.x",
+      });
+
+    template.resourceCountIs("AWS::Lambda::Function", 2);
 });
+
+
+/** 
+By setting the logRetention prop for our lambda function, 
+we created a custom CloudFormation resource.
+
+The custom resource is responsible for:
+
+1. creating a log group for the function, if one doesn't already exist
+2. updating the log retention period to the specified number of days
+
+If we look at the resources, our stack has provisioned, 
+we can see that there are 2 lambda functions - 
+ours and a custom resource, automatically created for us by CDK
+*/
